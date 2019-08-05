9152d1d0b90b945be7754366e2c860516b0d6acb
bobolen <bobolen8@gmail.com>
2019-08-05 10:34:42 -0600

    Just testing the commit and push

--- numbers.py
+++ numbers.py
@@ -24,3 +24,7 @@ print("Boolean const. bool([])", bool([]))
 print("Boolean const. bool([1, 2, 3])", bool([1, 2, 3]))
 print("Boolean const. bool("")", bool(""))
 print("Boolean const. bool('False')", bool("False"))
+# Relational Operators: ==, !=, <, >, >=, <=
+a = 12
+print("a == 12", a == 12)
+print("a != 12", a != 12)
