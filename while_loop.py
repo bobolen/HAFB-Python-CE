cce0b289a44373b82b80cba24ed5ab4dcb5c5559
bobolen <bobolen8@gmail.com>
2019-08-05 11:30:32 -0600

    String

--- while_loop.py
+++ while_loop.py
@@ -8,3 +8,19 @@ while counter != 0:
     print(counter)
     # Augmented Operator
     counter -= 1
+
+counter = 5
+while counter:
+    print(counter)
+    # Augmented Operator
+    counter -= 1
+
+print("Outside while loop")
+
+# Run forever
+while True:
+    print("Enter a number")
+    response = input()              # take user input
+    if int(response) % 7 == 0:      # number divisible by 7
+        break                       # exit loop
+
