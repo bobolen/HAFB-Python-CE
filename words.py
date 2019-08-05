0841c3b554fc530b62533f562fa200f9dd772814
bobolen <bobolen8@gmail.com>
2019-08-05 14:44:28 -0600

    Task 1 and 2 for words

---
+++ words.py
@@ -0,0 +1,24 @@
+"""
+Get a file from the web
+http://icarus.cs.weber.edu/~hvalle/hafb/words.txt
+
+Task 1: Count the number of words in document
+Task 2: Count the number of times each word appears in the file
+"""
+from urllib.request import urlopen
+file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+
+count = 0
+dictionary = {}
+with urlopen(file) as story:
+    for line in story:
+        words = line.decode('utf-8').split()    # split with space as separator
+        # print(words)
+        for word in words:
+            count += 1
+            if word in dictionary:
+                dictionary[word] += 1
+            else:
+                dictionary[word] = 1
+print("Total number of words is", count)
+print(dictionary)
