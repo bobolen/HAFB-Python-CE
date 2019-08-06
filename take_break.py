15ebdd0843cebf94244c5f44c02fb44269eb100d
bobolen <bobolen8@gmail.com>
2019-08-06 09:43:56 -0600

    finished take_break

---
+++ take_break.py
@@ -0,0 +1,24 @@
+"""
+Set an alarm to take a break by playing a youtube video
+"""
+import webbrowser
+import time
+
+
+def main():
+    """
+    Test function
+    :return: none
+    """
+    video_address = "https://youtu.be/W16bk86xIY0"
+    i = 3
+    while i:
+        # Delay "sleep"
+        time.sleep(60*60)
+        webbrowser.open(video_address)
+        i -= 1
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
