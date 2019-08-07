5efa19b8f789daecfba7cafea3379f9212f3a89b
bobolen <bobolen8@gmail.com>
2019-08-07 13:54:49 -0600

    Initial commit for my_sets and Hev_and_Bo

---
+++ Hev_and_Bo.py
@@ -0,0 +1,21 @@
+"""
+Trying to figure out if Hevynn hates Bo
+"""
+import random
+
+
+def main():
+    """
+    Test function
+    :return:
+    """
+    input("Do you think Hevynn hates you today? ")
+    input("Are you on Earth? ")
+    input("Is it before 4 pm? ")
+    print("Hevynn does not hate Bo")
+    print("Hevynn loves Bo")
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
